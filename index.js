5be501aade0d685ff250198864ab648ab0a6e7f3
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-17 14:47:57 +0530

    access token and refresh token

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken: ',accessToken);
+      console.log('refreshToken: ',refreshToken);
+      console.log('profile: ',profile);
     }
   )
 );
