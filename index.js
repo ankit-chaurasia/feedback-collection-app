fe94aa66e63ca13b161c564bf8dd9f960547dc32
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-17 14:25:47 +0530

    redirect uri and oauth callback

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ passport.use(
   )
 );
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
