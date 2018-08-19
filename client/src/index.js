d2e665ff500ada53a25439ceabd8c45c7928bae9
Ankit Chaurasia <ac13793@gmail.com>
2018-08-19 17:01:51 +0530

    finishing reducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 const provider = (
   <Provider store={store}>
