bad41d198e1b97c5569c4f6dc51d95fffb1910ea
Ankit Chaurasia <ac13793@gmail.com>
2018-08-19 21:40:04 +0530

    AuthReducer-Return-Values

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default (state = {}, action) => {
-  console.log(action);
+import { FETCH_USER } from '../actions/actionTypes';
+
+export default (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
