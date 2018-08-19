1e4c21e8f0c0f77e2c9ecb2bd17fb76a4af53b22
Ankit Chaurasia <ac13793@gmail.com>
2018-08-19 22:28:12 +0530

    Landing-Component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Emaily!</h1>
+    Collect feedback from your user
+  </div>
+);
+export default Landing;
