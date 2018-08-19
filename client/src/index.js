1a43afeff6cd30e413e58c3c86edef9968d63c1b
Ankit Chaurasia <ac13793@gmail.com>
2018-08-19 16:50:12 +0530

    Redux-Review-and-Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+const provider = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(provider, document.getElementById('root'));
 registerServiceWorker();
