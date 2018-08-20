acf3886f79be9e24abe9857d4dd89309195d036e
Ankit Chaurasia <ac13793@gmail.com>
2018-08-21 00:10:32 +0530

    stripe and BodyParser Middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,7 +10,7 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
