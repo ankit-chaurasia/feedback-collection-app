0611a9aef5aab855ccef6597618e0e0933fb74b5
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-21 16:15:03 +0530

    Routing in Production

--- index.js
+++ index.js
@@ -24,5 +24,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will server up production assests
+  // e.g. main.js, main.css
+  app.use(express.static('client/build'));
+  // Express will server up index.html file if it does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
