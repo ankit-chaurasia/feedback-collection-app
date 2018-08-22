3ac6e924e4016a4e422c46fc99c84907db52e96e
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-22 12:05:54 +0530

    Removed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -18,4 +17,3 @@ const provider = (
 );
 
 ReactDOM.render(provider, document.getElementById('root'));
-registerServiceWorker();
