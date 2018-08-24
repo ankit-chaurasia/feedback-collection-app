2389a18092b7964deeafe2f565aaed259647d8ea
Ankit Chaurasia <ac13793@gmail.com>
2018-08-24 21:54:49 +0530

    Mailer Constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,14 @@ const sendGrid = require('sendgrid');
 const { mail: helper } = sendGrid;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
