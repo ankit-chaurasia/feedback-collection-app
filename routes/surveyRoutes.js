af7e0ed2155eada9493eaa1d773e8c77f9d576fd
Ankit Chaurasia <ac13793@gmail.com>
2018-08-24 21:44:04 +0530

    Mailer in Use

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('survey');
 
@@ -15,5 +17,6 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,3 @@
+module.exports = ({ body }) => {
+  return '<div>' + body + '</div>';
+};
