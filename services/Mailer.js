6d85ce3657e420ac21af7689537ad7bf274c3e01
Ankit Chaurasia <ac13793@gmail.com>
2018-08-24 22:08:12 +0530

    Boilerplate for Sending Emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,7 +9,22 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    // This is inherited from sendGrid
+    this.addContent(this.body);
+    // Set click tracking
+    this.addClickTracking();
   }
+
+  formatAddresses = recipients =>
+    recipients.map(({ email }) => new helper.Email(email));
+
+  addClickTracking = () => {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  };
 }
 
 module.exports = Mailer;
