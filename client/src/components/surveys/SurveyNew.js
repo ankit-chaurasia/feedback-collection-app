a0aeb30358dec55968db737260316a3fac6822c7
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 18:46:04 +0530

    The-ReduxForm-Helper

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 export default class SurveyNew extends Component {
   render() {
-    return <div>Survey New!</div>;
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
   }
 }
