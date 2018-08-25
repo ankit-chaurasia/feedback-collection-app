5f03eee000278858294b96880ccab7e8c4cd4864
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 19:25:30 +0530

    DRYing-Up-Fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,9 +1,10 @@
 // SurveyField contains login to render a single label and text input
 import React from 'react';
 
-export default ({ input }) => {
+export default ({ input, label }) => {
   return (
     <div>
+      <label>{label}</label>
       <input {...input} />
     </div>
   );
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,7 +6,30 @@ class SurveyForm extends Component {
   renderFields = () => {
     return (
       <div>
-        <Field type="text" name="title" component={SurveyField} />
+        <Field
+          label="Survey Title"
+          type="text"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          label="Subject Line"
+          type="text"
+          name="subject"
+          component={SurveyField}
+        />
+        <Field
+          label="Email Body"
+          type="text"
+          name="body"
+          component={SurveyField}
+        />
+        <Field
+          label="Recipient List"
+          type="text"
+          name="emails"
+          component={SurveyField}
+        />
       </div>
     );
   };
