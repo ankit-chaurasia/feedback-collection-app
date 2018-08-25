03563bec424982e5abf22188c2135fd522a41acb
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 11:21:33 +0530

    Polish in the Route Handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,7 @@ const Survey = mongoose.model('survey');
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
-    const survey = await Survey({
+    const survey = Survey({
       title,
       subject,
       body,
@@ -18,6 +18,14 @@ module.exports = app => {
       dateSent: Date.now()
     });
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send({ err });
+    }
   });
 };
