e23df88c50aab24ff3b7218d6f30c0b551f28e77
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 19:11:03 +0530

    Custom-Field-Components

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains login to render a single label and text input
+import React from 'react';
+
+export default props => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
