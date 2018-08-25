e6c92c663bd68d5e135d27b7d5a27b6e68a1ca0e
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 11:09:18 +0530

    Improving the Email Template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,17 @@
 module.exports = ({ body }) => {
-  return '<div>' + body + '</div>';
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your input!</h3>
+          <p>Please anwser the following question:</p>
+          <p>${body}</p>
+          <div>
+            <a href="http://localhost:3000">Yes</a>
+            <a href="http://localhost:3000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
