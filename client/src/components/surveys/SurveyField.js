17582920418f1f0b124ba83725efd69b08604145
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 19:16:45 +0530

    Wiring-Up-Custom-Fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,10 @@
 // SurveyField contains login to render a single label and text input
 import React from 'react';
 
-export default props => {
+export default ({ input }) => {
   return (
     <div>
-      <input />
+      <input {...input} />
     </div>
   );
 };
