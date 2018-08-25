0c16d14e6523575616f84ebd079ffce68852ca56
Ankit Chaurasia <ac13793@gmail.com>
2018-08-25 23:33:29 +0530

    How-to-Toggle-Visibility

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,4 +1,4 @@
-const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export default emails => {
   const invalidEmails = emails
     .split(',')
