5252cb08b4d5d0f92996a74208a22f1fbd1caf35
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-27 12:29:04 +0530

    Outstanding Form Work

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = props => {
       Dashboard
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
-          <i className="large material-icons">chat_bubble_outline</i>
+          <i className="large material-icons">add</i>
         </Link>
       </div>
     </div>
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { submitSurvey } from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ label, name }) => {
     return (
       <div key={name}>
@@ -18,14 +19,26 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
         Back
       </button>
-      <button className="">Send</button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
     </div>
   );
 };
 
 const mapStateToProps = ({ form }) => ({ formValues: form.surveyForm.values });
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  { submitSurvey }
+)(SurveyFormReview);
