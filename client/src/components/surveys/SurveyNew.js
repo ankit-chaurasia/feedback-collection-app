6ca32675524077f09b9b5600055105208d4f3af4
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-27 12:41:51 +0530

    Dumping Form Values

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
-export default class SurveyNew extends Component {
+class SurveyNew extends Component {
   state = {
     showFormReview: false
   };
@@ -34,3 +35,7 @@ export default class SurveyNew extends Component {
     return <div>{this.renderContent()}</div>;
   }
 }
+
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
