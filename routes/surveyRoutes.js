c83728e048b75cebe16388fcb6edd6570e6392c5
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-28 11:54:26 +0530

    Code cleanup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -15,11 +15,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, event => {
-      const pathname = new URL(event.url).pathname;
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
       const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(pathname));
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
     });
+    console.log(events);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
