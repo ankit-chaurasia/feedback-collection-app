b16009fe418cb0fa5e421f77d7648b74b6973f55
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-08-28 12:22:20 +0530

    Lodash Chain Helper

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -15,23 +15,25 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
-      if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice
-        };
-      }
-    });
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    // Parse events
     // Remove null/undfined entries from events array
-    const compactEvents = _.compact(events);
     // Remove duplicate events based on email and surveyId
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-    console.log(uniqueEvents);
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+    console.log(events);
     res.send({});
   });
 
