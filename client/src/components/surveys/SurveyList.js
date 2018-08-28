f69f589a22c0ba0ac6970a15c34b05785bbb6c7f
Ankit Chaurasia <ac13793@gmail.com>
2018-08-28 22:20:42 +0530

    Reversing the Survey List

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,27 +7,29 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
   renderSurveys = () => {
-    return this.props.surveys.map(({ _id, title, body, dateSent, yes, no }) => {
-      return (
-        <div className="row" key={_id}>
-          <div className="col s12">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className="card-title">{title}</span>
-                <p>{body}</p>
-                <p className="right">
-                  Sent on: {new Date(dateSent).toLocaleDateString()}
-                </p>
-              </div>
-              <div className="card-action">
-                <a href="#">yes: {yes}</a>
-                <a href="#">No: {no}</a>
+    return this.props.surveys
+      .reverse()
+      .map(({ _id, title, body, dateSent, yes, no }) => {
+        return (
+          <div className="row" key={_id}>
+            <div className="col s12">
+              <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                  <span className="card-title">{title}</span>
+                  <p>{body}</p>
+                  <p className="right">
+                    Sent on: {new Date(dateSent).toLocaleDateString()}
+                  </p>
+                </div>
+                <div className="card-action">
+                  <a href="#">yes: {yes}</a>
+                  <a href="#">No: {no}</a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
   };
 
   render() {
