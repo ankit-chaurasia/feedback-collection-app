0101ce1881f01f7e6fdd5c418b25b09e03280144
Ankit Chaurasia <ac13793@gmail.com>
2018-08-28 22:16:56 +0530

    Rendering a List of Surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -6,9 +6,32 @@ class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+  renderSurveys = () => {
+    return this.props.surveys.map(({ _id, title, body, dateSent, yes, no }) => {
+      return (
+        <div className="row" key={_id}>
+          <div className="col s12">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">{title}</span>
+                <p>{body}</p>
+                <p className="right">
+                  Sent on: {new Date(dateSent).toLocaleDateString()}
+                </p>
+              </div>
+              <div className="card-action">
+                <a href="#">yes: {yes}</a>
+                <a href="#">No: {no}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
-    console.log(this.props.surveys);
-    return <div>Survey List</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
