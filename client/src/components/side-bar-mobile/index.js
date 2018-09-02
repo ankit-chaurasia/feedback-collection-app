2e2f6b89f55b980930f27623f87f07b9f574c482
Ankit Chaurasia <ac13793@gmail.com>
2018-09-02 19:21:47 +0530

    updated UI for mobile view

--- client/src/components/side-bar-mobile/index.js
+++ client/src/components/side-bar-mobile/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
   Button,
   Container,
   Icon,
   Menu,
   Segment,
-  Sidebar
+  Sidebar,
+  Image
 } from 'semantic-ui-react';
+import Payments from '../Payments';
+import './stylesheets/index.css';
 
 class SideBarMobile extends Component {
   state = {};
@@ -19,6 +24,48 @@ class SideBarMobile extends Component {
   handleToggle = () =>
     this.setState({ sidebarOpened: !this.state.sidebarOpened });
 
+  renderContent = () => {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <React.Fragment>
+            <Menu.Item>
+              <a href="/auth/google">Log in</a>
+            </Menu.Item>
+            <Menu.Item>
+              <a>Sign Up</a>
+            </Menu.Item>
+          </React.Fragment>
+        );
+      default:
+        return (
+          <React.Fragment>
+            <Menu.Item>
+              <Link to="/surveys/new">Create New Survey</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link to="/surveys">My Surveys</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <div style={{ margin: '0 10px' }}>
+                Credits: {this.props.auth.credits}
+              </div>
+            </Menu.Item>
+            <Menu.Item>
+              <Button as="a" inverted={!this.props.fixed} color="green">
+                <Payments />
+              </Button>
+            </Menu.Item>
+            <Menu.Item>
+              <a href="/api/logout">Logout</a>
+            </Menu.Item>
+          </React.Fragment>
+        );
+    }
+  };
+
   render() {
     const { sidebarOpened } = this.state;
     return (
@@ -30,14 +77,7 @@ class SideBarMobile extends Component {
           vertical
           visible={sidebarOpened}
         >
-          <Menu.Item as="a" active>
-            Home
-          </Menu.Item>
-          <Menu.Item as="a">Work</Menu.Item>
-          <Menu.Item as="a">Company</Menu.Item>
-          <Menu.Item as="a">Careers</Menu.Item>
-          <Menu.Item as="a">Log in</Menu.Item>
-          <Menu.Item as="a">Sign Up</Menu.Item>
+          {this.renderContent()}
         </Sidebar>
 
         <Sidebar.Pusher
@@ -48,31 +88,41 @@ class SideBarMobile extends Component {
           <Segment
             inverted
             textAlign="center"
-            style={{ minHeight: 350, padding: '1em 0em' }}
             vertical
+            style={{ padding: '0' }}
           >
             <Container>
               <Menu inverted pointing secondary size="large">
-                <Menu.Item onClick={this.handleToggle}>
+                <Menu.Item
+                  onClick={this.handleToggle}
+                  className="hamburgerIcon"
+                >
                   <Icon name="sidebar" />
                 </Menu.Item>
-                <Menu.Item position="right">
-                  <Button as="a" inverted>
-                    Log in
-                  </Button>
-                  <Button as="a" inverted style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
+                <Menu.Item header>
+                  <Link to="/" className="siteLogo">
+                    <Image
+                      size="mini"
+                      src={`${process.env.PUBLIC_URL}/favicon.ico`}
+                      style={{ marginRight: '1.5em' }}
+                    />
+                    <div className="title">EMAILY</div>
+                  </Link>
                 </Menu.Item>
               </Menu>
             </Container>
           </Segment>
-
-          {this.props.children}
+          <Container>
+            <Segment style={{ padding: '5em 0em' }} vertical>
+              {this.props.children}
+            </Segment>
+          </Container>
         </Sidebar.Pusher>
       </Sidebar.Pushable>
     );
   }
 }
 
-export default SideBarMobile;
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(mapStateToProps)(SideBarMobile);
