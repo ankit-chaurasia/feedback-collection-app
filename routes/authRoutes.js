ab440563d68f9ced0af3cf59775a41b7a5003e76
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-09-04 14:59:27 +0530

    updated scope in facebook auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,7 +19,7 @@ module.exports = app => {
   app.get(
     '/auth/facebook',
     passport.authenticate('facebook', {
-      scope: ['profile', 'email']
+      scope: ['public_profile', 'email']
     })
   );
 
